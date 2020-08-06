82f4713092b55ea2f08d394ed3546ca01fa86085
A_Little_Dragon <A_Little_Dragon@163.com>
2020-08-06 22:35:08 +0800

    add LICENSE and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SCAT',
+    version='',
+    packages=['SCAT'],
+    url='',
+    license='',
+    author='Jinglong Zhang',
+    author_email='',
+    description=''
+)
